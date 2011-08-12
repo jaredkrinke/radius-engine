58209a634a7faec4b8041a281ead98f8121b0fc7
Jared <deraj@users.sourceforge.net>
2011-08-12 08:31:16 -0700

    Capture into unique filenames

--- r_capture.c
+++ r_capture.c
@@ -30,85 +30,112 @@ THE SOFTWARE.
 #include "r_state.h"
 #include "r_capture.h"
 
+#define R_CAPTURE_FILENAME_TEMPLATE "Capture%d.%s"
+#define R_CAPTURE_EXTENSION_VIDEO   "rcv"
+#define R_CAPTURE_EXTENSION_AUDIO   "rca"
+#define R_CAPTURE_MAX_INDEX         999
+
 #define R_CAPTURE_PAGE_SIZE (32768)
 #define R_CAPTURE_FLUSH_SIZE    (4 * R_CAPTURE_PAGE_SIZE)
 #define R_CAPTURE_BUFFER_SIZE   (8 * R_CAPTURE_FLUSH_SIZE)
 
 r_status_t r_capture_start(r_state_t *rs, r_capture_t **capture_out)
 {
-    r_capture_t *capture = (r_capture_t*)malloc(sizeof(r_capture_t));
-    r_status_t status = (capture != NULL) ? R_SUCCESS : R_F_OUT_OF_MEMORY;
+    /* Find a new filename */
+    unsigned int filename_index = 1;
+    char filename_video[30];
+    char filename_audio[30];
+    r_status_t status = R_FAILURE;
 
+    for (filename_index = 1; filename_index <= R_CAPTURE_MAX_INDEX; ++filename_index)
+    {
+        sprintf(filename_video, R_CAPTURE_FILENAME_TEMPLATE, filename_index, R_CAPTURE_EXTENSION_VIDEO);
+        sprintf(filename_audio, R_CAPTURE_FILENAME_TEMPLATE, filename_index, R_CAPTURE_EXTENSION_AUDIO);
+
+        if (!PHYSFS_exists(filename_video) && !PHYSFS_exists(filename_audio))
+        {
+            status = R_SUCCESS;
+            break;
+        }
+    }
+
+    /* Now set up the capture */
     if (R_SUCCEEDED(status))
     {
-        /* Allocate video buffers */
-        capture->video_buffer_index = 0;
-        capture->video_buffers[0] = calloc(rs->video_width * rs->video_height, sizeof(int));
-        capture->video_buffers[1] = calloc(rs->video_width * rs->video_height, sizeof(int));
-        capture->video_packet_buffer = malloc(sizeof(r_capture_video_packet_t) + rs->video_width * rs->video_height * R_CAPTURE_FORMAT_RLE_MAX_SCALE);
+        r_capture_t *capture = (r_capture_t*)malloc(sizeof(r_capture_t));
 
-        status = (capture->video_buffers[0] != NULL && capture->video_buffers[1] != NULL && capture->video_packet_buffer != NULL) ? R_SUCCESS : R_F_OUT_OF_MEMORY;
+        status = (capture != NULL) ? R_SUCCESS : R_F_OUT_OF_MEMORY;
 
         if (R_SUCCEEDED(status))
         {
-            /* Start video log */
-            /* TODO: Don't hard-code paths */
-            status = r_stream_async_start(&capture->video_log, "Capture.rcv", R_CAPTURE_BUFFER_SIZE, R_CAPTURE_FLUSH_SIZE);
+            /* Allocate video buffers */
+            capture->video_buffer_index = 0;
+            capture->video_buffers[0] = calloc(rs->video_width * rs->video_height, sizeof(int));
+            capture->video_buffers[1] = calloc(rs->video_width * rs->video_height, sizeof(int));
+            capture->video_packet_buffer = malloc(sizeof(r_capture_video_packet_t) + rs->video_width * rs->video_height * R_CAPTURE_FORMAT_RLE_MAX_SCALE);
+
+            status = (capture->video_buffers[0] != NULL && capture->video_buffers[1] != NULL && capture->video_packet_buffer != NULL) ? R_SUCCESS : R_F_OUT_OF_MEMORY;
 
             if (R_SUCCEEDED(status))
             {
-                r_capture_header_t video_log_header = { r_capture_signature, R_CAPTURE_TYPE_VIDEO, SDL_GetTicks(), { R_CAPTURE_VIDEO_CODEC_DIFF_RLE, rs->video_width, rs->video_height } };
-
-                status = r_stream_async_write(&capture->video_log, sizeof(r_capture_header_t), &video_log_header);
+                /* Start video log */
+                status = r_stream_async_start(&capture->video_log, filename_video, R_CAPTURE_BUFFER_SIZE, R_CAPTURE_FLUSH_SIZE);
 
                 if (R_SUCCEEDED(status))
                 {
-                    /* Start audio log */
-                    status = r_stream_async_start(&capture->audio_log, "Capture.rca", R_CAPTURE_BUFFER_SIZE, R_CAPTURE_FLUSH_SIZE);
+                    r_capture_header_t video_log_header = { r_capture_signature, R_CAPTURE_TYPE_VIDEO, SDL_GetTicks(), { R_CAPTURE_VIDEO_CODEC_DIFF_RLE, rs->video_width, rs->video_height } };
+
+                    status = r_stream_async_write(&capture->video_log, sizeof(r_capture_header_t), &video_log_header);
 
                     if (R_SUCCEEDED(status))
                     {
-                        r_capture_header_t audio_log_header = { r_capture_signature, R_CAPTURE_TYPE_AUDIO, SDL_GetTicks(), { R_CAPTURE_AUDIO_CODEC_RAW } };
-
-                        status = r_stream_async_write(&capture->audio_log, sizeof(r_capture_header_t), &audio_log_header);
+                        /* Start audio log */
+                        status = r_stream_async_start(&capture->audio_log, filename_audio, R_CAPTURE_BUFFER_SIZE, R_CAPTURE_FLUSH_SIZE);
 
                         if (R_SUCCEEDED(status))
                         {
-                            *capture_out = capture;
-                        }
-                            
-                        if (R_FAILED(status))
-                        {
-                            r_stream_async_stop(&capture->audio_log);
+                            r_capture_header_t audio_log_header = { r_capture_signature, R_CAPTURE_TYPE_AUDIO, SDL_GetTicks(), { R_CAPTURE_AUDIO_CODEC_RAW } };
+
+                            status = r_stream_async_write(&capture->audio_log, sizeof(r_capture_header_t), &audio_log_header);
+
+                            if (R_SUCCEEDED(status))
+                            {
+                                *capture_out = capture;
+                            }
+                                
+                            if (R_FAILED(status))
+                            {
+                                r_stream_async_stop(&capture->audio_log);
+                            }
                         }
                     }
-                }
 
-                if (R_FAILED(status))
+                    if (R_FAILED(status))
+                    {
+                        r_stream_async_stop(&capture->video_log);
+                    }
+                }
+            }
+
+            if (R_FAILED(status))
+            {
+                if (capture->video_packet_buffer != NULL)
                 {
-                    r_stream_async_stop(&capture->video_log);
+                    free(capture->video_packet_buffer);
                 }
-            }
-        }
 
-        if (R_FAILED(status))
-        {
-            if (capture->video_packet_buffer != NULL)
-            {
-                free(capture->video_packet_buffer);
-            }
+                if (capture->video_buffers[1] != NULL)
+                {
+                    free(capture->video_buffers[0]);
+                }
 
-            if (capture->video_buffers[1] != NULL)
-            {
-                free(capture->video_buffers[0]);
-            }
+                if (capture->video_buffers[0] != NULL)
+                {
+                    free(capture->video_buffers[1]);
+                }
 
-            if (capture->video_buffers[0] != NULL)
-            {
-                free(capture->video_buffers[1]);
+                free(capture);
             }
-
-            free(capture);
         }
     }
 
